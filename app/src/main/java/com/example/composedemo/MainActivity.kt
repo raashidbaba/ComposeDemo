3e75c4a38176360370d408efeeb0504ba7f99be7
raashid <raashid@kayana.co.uk>
2025-01-04 14:29:13 +0530

    initial commit

---
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.composedemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composedemo.ui.theme.ComposeDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+        setContent {
+            Text(text = "hello josh")
+        }
+    }
+}
+
+
+@Composable
+fun SayCheesy(name: String = "cheesy"){
+    Text(text = "hello $name")
+}
+
+
+
+@Preview(showBackground = true, name = "a1", showSystemUi = true )
+@Composable
+fun PreviewFunction(){
+        SayCheesy("world")
+}
